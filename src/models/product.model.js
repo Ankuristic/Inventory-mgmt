58b2bc70ea614de36c16dc39496513c22664f02e
Ankuristic <63995112+Ankuristic@users.noreply.github.com>
2023-12-15 02:45:31 +0530

    first commit

---
+++ src/models/product.model.js
@@ -0,0 +1,28 @@
+// Import the necessary modules here
+import path from 'path'
+import ProductModel from ""
+
+
+export default class ProductModel {
+  fetchProducts = () => {
+    // Write your code here
+
+    constructor(_id,_name,_desc,_price,_imageUel){
+
+
+      this.id=_id;
+      this.name=_name;
+      this.desc=_desc;
+      this.price=_price
+      this._imageUel=imageUrl
+
+
+    }
+
+
+    static get(){
+      return products;
+    }
+
+  };
+}
